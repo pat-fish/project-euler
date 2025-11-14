44a18fa5c4dbe57e72fcf82a3ed5255b8eb248af
Patrick Fish <patrickfish10@icloud.com>
2025-11-14 13:42:57 -0500

    solution for pr 5

---
+++ pr5/problem5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i = 20;
+    while (1) {
+        if (i % 11 == 0 && 
+            i % 12 == 0 && 
+            i % 13 == 0 && 
+            i % 14 == 0 && 
+            i % 16 == 0 && 
+            i % 17 == 0 && 
+            i % 18 == 0 && 
+            i % 19 == 0 && 
+            i % 20 == 0) {
+            cout << i << endl;
+            return 0;
+        } else {
+            i++;
+        }
+    }
+}
+
